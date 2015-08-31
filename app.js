e65d0f60c4d6e0ac9f04ef09bf5ccedf1b26fc70
Whitney Turland <wlturland@gmail.com>
2015-08-31 11:24:50 -0500

    Displaying lists

--- app.js
+++ app.js
@@ -3,4 +3,11 @@ angular.module('flapperNews', [])
 '$scope',
 function($scope){
 	$scope.test = 'Hello world!';
+	$scope.posts = [
+		{title: 'post 1', upvotes: 5},
+		{title: 'post 2', upvotes: 2},
+		{title: 'post 3', upvotes: 15},
+		{title: 'post 4', upvotes: 9},
+		{title: 'post 5', upvotes: 4}
+		];
 }]);
